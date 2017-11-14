b3d589352cc01ccc4f35e32066a752c0ebd630b9
orpheus92 <dwang0127@gmail.com>
2017-11-13 17:55:34 -0700

    Fix the bug for the merging process

--- alg.py
+++ alg.py
@@ -13,22 +13,27 @@ def convertkey(ind1,ind2):
 def mergemin(c,p,d):
     indpair = list(d.keys())
     indpair2 = np.array([convertind(pair) for pair in indpair ])
-    listmax = indpair2[:,1][indpair2[:,0]==int(p)]
+    listmax = indpair2[:,1][indpair2[:,0]==int(c)]
     for maxin in listmax:
-        if convertkey(int(c),maxin) in indpair:
+        if convertkey(int(p),maxin) in indpair:
             temp = d[convertkey(int(c),maxin)]
             d[convertkey(int(p),maxin)]=d[convertkey(int(p),maxin)]+temp
             del d[convertkey(int(c),maxin)]
+        else:
+            d[convertkey(int(p),maxin)] = d.pop(convertkey(int(c),maxin))
+
 
 def mergemax(c,p,d):
     indpair = list(d.keys())
     indpair2 = np.array([convertind(pair) for pair in indpair ])
-    listmin = indpair2[:,0][indpair2[:,1]==int(p)]
+    listmin = indpair2[:,0][indpair2[:,1]==int(c)]
     for minin in listmin:
-        if convertkey(minin,int(c)) in indpair:
+        if convertkey(minin,int(p)) in indpair:
             temp = d[convertkey(minin,int(c))]
             d[convertkey(minin,int(p))]=d[convertkey(minin,int(p))]+temp
             del d[convertkey(minin,int(c))]
+        else:
+            d[convertkey(minin, int(p))] = d.pop(convertkey(minin, int(c)))
 
 maxmerge = np.genfromtxt('max_merge.csv', delimiter=",")
 minmerge = np.genfromtxt('min_merge.csv', delimiter=",")
@@ -57,7 +62,7 @@ with open('persistence3.json') as data_file:
 #print("Persistence Levels = ")
 #print("[0,"+str(max(max(maxmerge[:,0]),max(minmerge[:,0])))+"]")
 #Pinter = float(input('Input Persistence of Interest: '))
-Pinter = 2
+Pinter = 8
 
 # minindex to from child to parent
 #minchild = minmerge2[minmerge2[:,0]<Pinter][:,2]
@@ -76,11 +81,15 @@ newdict = data.copy()
 [r,c] = tomerge.shape
 
 for i in range(r):
+    #print('cur Partitions = '+str(len(newdict)))
+    #print('P value = '+ str(tomerge[i,0]))
+    #print(int(child[i]))
+    #print(int(parent[i]))
     if tomerge[i,1]==0:
         mergemin(int(child[i]),int(parent[i]),newdict)
     else:
         mergemax(int(child[i]),int(parent[i]),newdict)
-
+print(len(newdict))
 print(newdict)
 #print(indpair2)
 #print(child)
