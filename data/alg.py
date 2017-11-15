9db74941135a57fec5e5931576785348a1fc2702
orpheus92 <dwang0127@gmail.com>
2017-11-15 10:22:41 -0700

    Generate new json files for direct use to construct tree

--- data/alg.py
+++ data/alg.py
@@ -11,30 +11,45 @@ def convertkey(ind1,ind2):
     return str(ind1)+', '+str(ind2)
 
 def mergemin(c,p,d):
+    outlist = []
     indpair = list(d.keys())
     indpair2 = np.array([convertind(pair) for pair in indpair ])
     listmax = indpair2[:,1][indpair2[:,0]==int(c)]
     for maxin in listmax:
         if convertkey(int(p),maxin) in indpair:
+            #print('Parent:'+convertkey(int(p),maxin)+' Child:'+convertkey(int(c),maxin))
             temp = d[convertkey(int(c),maxin)]
             d[convertkey(int(p),maxin)]=d[convertkey(int(p),maxin)]+temp
             del d[convertkey(int(c),maxin)]
+            outlist.append(convertkey(int(p),maxin))
+            outlist.append(convertkey(int(c),maxin))
+            #return [convertkey(int(p),maxin),convertkey(int(c),maxin)]
         else:
             d[convertkey(int(p),maxin)] = d.pop(convertkey(int(c),maxin))
-
-
+            #print('Parent:'+ convertkey(int(p),maxin)+' Child:'+convertkey(int(c),maxin))
+            outlist.append(convertkey(int(p),maxin))
+            outlist.append(convertkey(int(c),maxin))
+    return outlist
+            #return [convertkey(int(p),maxin),convertkey(int(c),maxin)]
 def mergemax(c,p,d):
+    outlist = []
     indpair = list(d.keys())
     indpair2 = np.array([convertind(pair) for pair in indpair ])
     listmin = indpair2[:,0][indpair2[:,1]==int(c)]
     for minin in listmin:
         if convertkey(minin,int(p)) in indpair:
+            #print('Parent:'+convertkey(minin,int(p))+' Child:'+convertkey(minin,int(c)))
             temp = d[convertkey(minin,int(c))]
             d[convertkey(minin,int(p))]=d[convertkey(minin,int(p))]+temp
             del d[convertkey(minin,int(c))]
+            outlist.append(convertkey(minin,int(p)))
+            outlist.append(convertkey(minin,int(c)))
         else:
             d[convertkey(minin, int(p))] = d.pop(convertkey(minin, int(c)))
-
+            outlist.append(convertkey(minin, int(p)))
+            outlist.append(convertkey(minin, int(c)))
+            #print('Parent:'+convertkey(minin, int(p))+' Child:'+convertkey(minin,int(c)))
+    return outlist
 maxmerge = np.genfromtxt('max_merge.csv', delimiter=",")
 minmerge = np.genfromtxt('min_merge.csv', delimiter=",")
 # Explain how the partitions merge
@@ -50,7 +65,7 @@ totalmerge = np.concatenate((maxmerge, minmerge), axis=0)
 #minmerge2 = minmerge[np.argsort(minmerge[:, 0])]
 
 totalmerge2 = totalmerge[np.argsort(totalmerge[:, 0])]
-print(totalmerge2);
+#print(totalmerge2);
 # Json file that stores the initial partition
 # Keys specified as "minInd, maxInd",
 # Values store the points that belong to the partition
@@ -62,8 +77,8 @@ with open('persistence3.json') as data_file:
 
 #print("Persistence Levels = ")
 #print("[0,"+str(max(max(maxmerge[:,0]),max(minmerge[:,0])))+"]")
-#Pinter = float(input('Input Persistence of Interest: '))
-Pinter = 8
+Pinter = float(input('Input Persistence of Interest: '))
+#Pinter = 8
 
 # minindex to from child to parent
 #minchild = minmerge2[minmerge2[:,0]<Pinter][:,2]
@@ -80,17 +95,39 @@ newdict = data.copy()
 #indpair = list(data.keys())
 #indpair2 = np.array([convertind(pair) for pair in indpair ])
 [r,c] = tomerge.shape
-
+perdict = {}
 for i in range(r):
     #print('cur Partitions = '+str(len(newdict)))
     #print('P value = '+ str(tomerge[i,0]))
     #print(int(child[i]))
     #print(int(parent[i]))
+    print('Plevel = '+str(tomerge[i,0]))
     if tomerge[i,1]==0:
-        mergemin(int(child[i]),int(parent[i]),newdict)
+
+        perdict[tomerge[i,0]] = mergemin(int(child[i]),int(parent[i]),newdict)
     else:
-        mergemax(int(child[i]),int(parent[i]),newdict)
-print(len(newdict))
-print(newdict)
-#print(indpair2)
-#print(child)
+        perdict[tomerge[i,0]] = mergemax(int(child[i]),int(parent[i]),newdict)
+
+plist = tomerge[:,0]
+pre = plist[-1]
+curlist = []
+treedata = {}
+treedata[-1]=[perdict[tomerge[-1,0]][0]]
+print(type(treedata))
+print(type(perdict))
+#print(perdict)
+for i in reversed(plist):
+    curlist = set(perdict[i]+list(curlist))
+    print(curlist)
+    treedata[i]=list(curlist)
+    pre = i
+    #parent = perdict[i][0::2]
+    #child = perdict[i][1::2]
+    #print(parent)
+    #print(child)
+    #print('======================')
+with open('treedata.json', 'w') as fp:
+    json.dump(treedata, fp)
+
+with open('mergetree.json', 'w') as fp:
+    json.dump(perdict, fp)
