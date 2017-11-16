bc2ba8ec72094bf388c93ea9bc64d31ea2c01f21
orpheus92 <dwang0127@gmail.com>
2017-11-15 23:55:52 -0700

    Modified merging code

--- data/alg.py
+++ data/alg.py
@@ -1,5 +1,8 @@
 import numpy as np
 import json
+import pandas as pd
+
+import csv
 import time
 import os
 
@@ -7,10 +10,24 @@ def convertind(str):
     str1,str2 = str.split(',')
     return [int(str1),int(str2)]
 
+def convert(str):
+    newstr = []
+    for i in str:
+        str1,str2,str3 = i.split(',')
+        newstr.append(convertkey(int(str1), int(str2)))
+    return newstr
+
+def convt(stra, ind):
+
+    return(stra+', '+str(ind))
+
 def convertkey(ind1,ind2):
     return str(ind1)+', '+str(ind2)
 
-def mergemin(c,p,d):
+def convertkey3(ind1,ind2,ind3):
+    return str(ind1)+', '+str(ind2)+', '+str(ind3)
+
+def mergemin(c,p,d,per):
     outlist = []
     indpair = list(d.keys())
     indpair2 = np.array([convertind(pair) for pair in indpair ])
@@ -21,17 +38,23 @@ def mergemin(c,p,d):
             temp = d[convertkey(int(c),maxin)]
             d[convertkey(int(p),maxin)]=d[convertkey(int(p),maxin)]+temp
             del d[convertkey(int(c),maxin)]
-            outlist.append(convertkey(int(p),maxin))
-            outlist.append(convertkey(int(c),maxin))
+            outlist.append(convertkey3(int(p),maxin,per-1))
+            outlist.append(convertkey3(int(c),maxin,per))
+            #outlist.append(convertkey3(int(p), maxin, per-1))
+            #outlist.append(convertkey3(int(p), maxin, per))
+
+
             #return [convertkey(int(p),maxin),convertkey(int(c),maxin)]
         else:
             d[convertkey(int(p),maxin)] = d.pop(convertkey(int(c),maxin))
             #print('Parent:'+ convertkey(int(p),maxin)+' Child:'+convertkey(int(c),maxin))
-            outlist.append(convertkey(int(p),maxin))
-            outlist.append(convertkey(int(c),maxin))
+            outlist.append(convertkey3(int(p),maxin,per-1))
+            outlist.append(convertkey3(int(c),maxin,per))
+            #outlist.append(convertkey3(int(p), maxin, per-1))
+            #outlist.append(convertkey3(int(p), maxin, per))
     return outlist
             #return [convertkey(int(p),maxin),convertkey(int(c),maxin)]
-def mergemax(c,p,d):
+def mergemax(c,p,d,per):
     outlist = []
     indpair = list(d.keys())
     indpair2 = np.array([convertind(pair) for pair in indpair ])
@@ -42,12 +65,16 @@ def mergemax(c,p,d):
             temp = d[convertkey(minin,int(c))]
             d[convertkey(minin,int(p))]=d[convertkey(minin,int(p))]+temp
             del d[convertkey(minin,int(c))]
-            outlist.append(convertkey(minin,int(p)))
-            outlist.append(convertkey(minin,int(c)))
+            outlist.append(convertkey3(minin,int(p),per-1))
+            outlist.append(convertkey3(minin,int(c),per))
+            #outlist.append(convertkey3(minin, int(p), per-1))
+            #outlist.append(convertkey3(minin, int(p), per))
         else:
             d[convertkey(minin, int(p))] = d.pop(convertkey(minin, int(c)))
-            outlist.append(convertkey(minin, int(p)))
-            outlist.append(convertkey(minin, int(c)))
+            outlist.append(convertkey3(minin, int(p),per-1))
+            outlist.append(convertkey3(minin, int(c),per))
+            #outlist.append(convertkey3(minin, int(p), per-1))
+            #outlist.append(convertkey3(minin, int(p), per))
             #print('Parent:'+convertkey(minin, int(p))+' Child:'+convertkey(minin,int(c)))
     return outlist
 maxmerge = np.genfromtxt('max_merge.csv', delimiter=",")
@@ -61,31 +88,16 @@ minmerge[:,1] = 0 # 0 for minima
 # sort to avoid wrong merge
 
 totalmerge = np.concatenate((maxmerge, minmerge), axis=0)
-#maxmerge2 = maxmerge[np.argsort(maxmerge[:, 0])]
-#minmerge2 = minmerge[np.argsort(minmerge[:, 0])]
+
 
 totalmerge2 = totalmerge[np.argsort(totalmerge[:, 0])]
-#print(totalmerge2);
 # Json file that stores the initial partition
 # Keys specified as "minInd, maxInd",
 # Values store the points that belong to the partition
 with open('persistence3.json') as data_file:
     data = json.load(data_file)
-#print(data)
-# suppose the user input a persistence level, we need an algorithm to update
-# the clusters so that the trees show results of specific persistence level
 
-#print("Persistence Levels = ")
-#print("[0,"+str(max(max(maxmerge[:,0]),max(minmerge[:,0])))+"]")
-#Pinter = float(input('Input Persistence of Interest: '))
 Pinter = 8
-
-# minindex to from child to parent
-#minchild = minmerge2[minmerge2[:,0]<Pinter][:,2]
-#minparent = minmerge2[minmerge2[:,0]<Pinter][:,3]
-
-#maxchild = maxmerge2[maxmerge2[:,0]<Pinter][:,2]
-#maxparent = maxmerge2[maxmerge2[:,0]<Pinter][:,3]
 
 child = totalmerge2[totalmerge2[:,0]<Pinter][:,2]
 parent = totalmerge2[totalmerge2[:,0]<Pinter][:,3]
@@ -98,40 +110,44 @@ newdict = data.copy()
 perdict = {}
 totallist = [];
 for i in range(r):
-    #print('cur Partitions = '+str(len(newdict)))
-    #print('P value = '+ str(tomerge[i,0]))
-    #print(int(child[i]))
-    #print(int(parent[i]))
-    print('Plevel = '+str(tomerge[i,0]))
+
+    #print('Plevel = '+str(tomerge[i,0]))
     if tomerge[i,1]==0:
+        perdict[tomerge[i,0]] = mergemin(int(child[i]),int(parent[i]),newdict,r-i)
+        ##perdict[tomerge[i,0]] = mergemin(int(child[i]),int(parent[i]),newdict,r-i)
 
-        perdict[tomerge[i,0]] = mergemin(int(child[i]),int(parent[i]),newdict)
     else:
-        perdict[tomerge[i,0]] = mergemax(int(child[i]),int(parent[i]),newdict)
+        perdict[tomerge[i,0]] = mergemax(int(child[i]),int(parent[i]),newdict,r-i)
+        ##perdict[tomerge[i,0]] = mergemax(int(child[i]),int(parent[i]),newdict,r-i)
 
+    #print(perdict[tomerge[i, 0]])
 plist = tomerge[:,0]
 pre = plist[-1]
 curlist = []
 treedata = {}
-treedata[-1]=[perdict[tomerge[-1,0]][0]]
-#print(type(treedata))
-#print(type(perdict))
-#print(perdict)
-for i in reversed(plist):
-    curlist = set(perdict[i]+list(curlist))
+treedata[0]=convert([perdict[tomerge[-1,0]][0]])
+total = len(plist)
+for ind,i in reversed(list(enumerate(plist))):
+    curlist = set(convert(perdict[i])+list(curlist))
     #print(curlist)
-    treedata[i]=list(curlist)
+    treedata[total-ind]=list(curlist)
     pre = i
     totallist = totallist + perdict[i]
-    #parent = perdict[i][0::2]
-    #child = perdict[i][1::2]
-    #print(parent)
-    #print(child)
-    #print('======================')
-#with open('treedata.json', 'w') as fp:
-#json.dump(treedata, fp)
+
+
+#print(totallist)
+#with open('totaltree.json', 'w') as fp:
+#    json.dump(treedata, fp)
+num = len(treedata)
+for i in range(num):
+    clist = treedata[i]
+    for j in clist:
+        totallist = totallist + [convt(j,i),convt(j,i+1)]
+#print(totallist)
 mlist = np.array(totallist)
-print(mlist)
-print(mlist.reshape(int(len(mlist)/2),2))
-#with open('mergetree.json', 'w') as fp:
-#json.dump(perdict, fp)
+##print(mlist)
+pclist = mlist.reshape(int(len(mlist)/2),2)
+print(pclist)
+
+df = pd.DataFrame(pclist)
+df.to_csv("Allmerge.csv",header=None,index=False)
