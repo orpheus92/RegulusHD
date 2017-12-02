7c2f1f40b30dd1d7e965e4f9a45135482b915718
orpheus92 <dwang0127@gmail.com>
2017-12-01 22:48:16 -0700

    Minor change on boxplot and histogram to show all attributes

--- js/Plot.js
+++ js/Plot.js
@@ -343,7 +343,7 @@ class Plots {
             }
         }
 
-        for (let i = 0; i < datacol - 1; i++) {
+        for (let i = 0; i < datacol ; i++) {
             let groupCount = obj[attr[i]];
             groupCount.sort(function (a, b) {
                 return a - b
@@ -456,7 +456,7 @@ class Plots {
 
         let numOfBins = 10;
 
-        for (let i = 0; i < datacol - 1; i++) {
+        for (let i = 0; i < datacol; i++) {
             let curData = [];
             for (let j = 0; j < datarow; j++) {
                 curData.push(parseFloat(data[j][attr[i]]));
